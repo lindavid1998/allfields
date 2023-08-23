e3b17745a8350712d4f49d28a8f0a50e705dea4c
David Lin <lindavid1998@gmail.com>
2023-08-23 08:35:31 -0700

    Create Navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../Button';
+import Avatar from '../Avatar';
+
+const Wrapper = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+  height: 64px;
+	width: 100%;
+  padding: 0 16px;
+  gap: 10px;
+`;
+
+const StyledH1 = styled.h1`
+  margin-right: auto;
+	color: var(--main-text-color);
+`
+
+const Navbar = () => {
+	return (
+		<Wrapper>
+			<StyledH1>AllFields</StyledH1>
+			<Button
+				text='Try AllFields+ for free'
+				color='bright'
+				size='small'
+			></Button>
+      <Button text='Log in' color='light' size='small'></Button>
+      <Avatar></Avatar>
+		</Wrapper>
+	);
+};
+
+export default Navbar;
