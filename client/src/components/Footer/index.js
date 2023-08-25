ff0f6aa8f6139fd8fe2dc20d1d8f6b1e5df132c7
David Lin <lindavid1998@gmail.com>
2023-08-24 22:37:44 -0700

    Add LinkedIn logo to Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-import githubLogo from '../../assets/github-mark-white.png'
+import githubLogo from '../../assets/github-mark-white.png';
+import linkedinLogo from '../../assets/linkedin-logo.svg';
 
 const Wrapper = styled.div`
-	background-color: var(--dark-bg-color);
 	width: 100%;
+	background-color: var(--dark-bg-color);
 	padding: var(--home-padding);
 `;
 
-const Subwrapper = styled.div`
-  display: flex;
-  gap: 20px;
-  align-ites: center;
-`
+const Content = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: end;
+	gap: 20px;
+	max-width: var(--max-width);
+	margin: 0 auto;
+`;
 
 const Text = styled.div`
 	color: #fff;
 	font-weight: bold;
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 `;
 
 const Img = styled.img`
-  height: 30px;
-  width: auto;
-  cursor: pointer;
-`
+	height: 30px;
+	width: auto;
+	cursor: pointer;
+`;
 
 const Footer = () => {
 	return (
 		<Wrapper>
-			<Subwrapper>
+			<Content>
 				<Text>A Personal Project by David Lin</Text>
-				<Img src={githubLogo} />
-			</Subwrapper>
+				<a href='https://www.linkedin.com/in/lindavid1998/'>
+					<Img src={linkedinLogo} />
+				</a>
+				<a href='https://github.com/lindavid1998'>
+					<Img src={githubLogo} />
+				</a>
+			</Content>
 		</Wrapper>
 	);
 };
