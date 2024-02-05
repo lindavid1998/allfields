41cf5ae00901476324911aaa5b86fc21b1e9285d
David Lin <lindavid1998@gmail.com>
2024-02-04 18:54:32 -0800

    Improve styling of auth status page

--- client/src/pages/AuthStatus/index.js
+++ client/src/pages/AuthStatus/index.js
@@ -9,20 +9,36 @@ import { PathContext } from '../../utils/utils.js';
 const Content = styled.div`
 	width: 100%;
 	height: 100vh;
-	background-image: url(${backgroundImage});
 	display: flex;
-	flex-direction: column;
-	align-items: center;
+	align-items: flex-start;
 	padding: 20px;
 	background-size: cover;
 	background-repeat: no-repeat;
 	background-position: center;
+	text-align: left;
+	max-width: var(--max-width);
 `;
 
-const Div = styled.div`
+const Text = styled.div`
 	display: flex;
 	flex-direction: column;
-	align-items: center;
+	align-items: left;
+	margin-right: 20px;
+	margin-top: 50px;
+	width: 50%;
+`;
+
+const ImgContainer = styled.div`
+	border-radius: 40px;
+	width: 450px;
+	height: 750px; 
+	overflow: hidden;
+`;
+
+const Img = styled.img`
+	height: 100%;
+	width: 100%;
+	object-fit: cover;
 `;
 
 const AuthStatus = () => {
@@ -45,7 +61,7 @@ const AuthStatus = () => {
 		return () => unsubscribe();
 	}, []);
 
-	// route to landing page once timer is up
+	// route to previous page once timer is up
 	useEffect(() => {
 		if (timer <= 0) {
 			navigate(redirectPath);
@@ -55,13 +71,17 @@ const AuthStatus = () => {
 
 	return (
 		<Content>
-			<Div>
+			<Text>
 				<h4>
 					You have been signed {status}. Back to{' '}
 					<a href={redirectPath}>previous page</a>.
 				</h4>
 				<p>You will be automatically redirected in {timer} seconds.</p>
-			</Div>
+			</Text>
+
+			<ImgContainer>
+				<Img src={backgroundImage} />
+			</ImgContainer>
 		</Content>
 	);
 };
